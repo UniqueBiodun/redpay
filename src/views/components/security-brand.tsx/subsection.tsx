44d84c4d224c1da0831a123bfb356313a5b753e4
Abiodun Adu <abiodun.adu@iisysgroup.com>
2023-09-28 15:20:48 +0100

    updated div animations

--- src/views/components/security-brand.tsx/subsection.tsx
+++ src/views/components/security-brand.tsx/subsection.tsx
@@ -1,6 +1,8 @@
 import React, { ReactNode, useEffect } from "react";
-import { motion, useAnimation } from "framer-motion";
-import { useInView } from "react-intersection-observer";
+// import { useAnimation } from "framer-motion";
+// import { useInView } from "react-intersection-observer";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 interface TextImgContainerProps {
 	addStyle?: string;
@@ -13,34 +15,41 @@ const SubSection: React.FC<TextImgContainerProps> = ({
 	firstDiv,
 	secondDiv,
 }: TextImgContainerProps) => {
-	const [ref, inView] = useInView({
-		threshold: 0.01, // Percentage of element visible to trigger
-	});
+	// const [ref, inView] = useInView({
+	// 	threshold: 0.2, // Percentage of element visible to trigger
+	// });
 
-	const animation = useAnimation();
+	// const animation = useAnimation();
+
+	// useEffect(() => {
+	// 	if (inView) {
+	// 		animation.start({
+	// 			x: 0,
+	// 			opacity: 1,
+	// 			transition: {
+	// 				type: "smooth",
+	// 				duration: 2,
+	// 			},
+	// 		});
+	// 	}
+	// 	if (!inView) {
+	// 		animation.start({
+	// 			x: "-100%",
+	// 			opacity: 0,
+	// 			transition: {
+	// 				type: "tween",
+	// 				duration: 3,
+	// 			},
+	// 		});
+	// 	}
+	// }, [inView, animation]);
 
 	useEffect(() => {
-		if (inView) {
-			animation.start({
-				x: 0,
-				opacity: 1,
-				transition: {
-					type: "smooth",
-					duration: 2,
-				},
-			});
-		}
-		if (!inView) {
-			animation.start({
-				x: "-100%",
-				opacity: 0,
-				transition: {
-					type: "tween",
-					duration: 3,
-				},
-			});
-		}
-	}, [inView, animation]);
+		AOS.init({
+			duration: 1000, // Animation duration (in milliseconds)
+			once: false, // Only run the animation once
+		});
+	}, []);
 
 	return (
 		<>
@@ -51,14 +60,12 @@ const SubSection: React.FC<TextImgContainerProps> = ({
 					className={`container pl-0 h-full mx-auto flex flex-col-reverse lg:flex-row justify-between  gap-6`}
 				>
 					{/* First Div */}
-					<motion.div
-						ref={ref}
-						initial={{ x: "-100%", opacity: 0 }}
-						animate={animation}
+					<div
+						data-aos="fade-right"
 						className="w-full flex flex-row items-center justify-center bg-[#FDFDFD] border border-[#E1E5EA] rounded-tr rounded-br text-secondary-400 px-4 md:px-[60px] py-[52px]"
 					>
 						{firstDiv}
-					</motion.div>
+					</div>
 
 					{/* Second Div */}
 					<div className="w-full flex items-center">
