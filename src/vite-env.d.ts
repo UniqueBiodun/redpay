c38efc9a800a6b7499d8c75507caa517db372b79
UniqueBiodun <aduabiodunemmanuel@gmail.com>
2023-10-17 20:13:50 +0100

    updates: vite-env updated

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+	readonly VITE_APP_MERCHANT_SIGNUP: string;
+	readonly VITE_APP_MERCHANT_SIGNIN: string;
+	// more env variables...
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
